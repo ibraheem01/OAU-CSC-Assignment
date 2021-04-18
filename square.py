ca4cac0856bb2c4ce8451c683b2281ccc5df7b9b
ibraheem01 <66010073+ibraheem01@users.noreply.github.com>
2021-04-19 00:59:19 +0100

    Initial commit

---
+++ square.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 18 23:57:47 2021
+
+@author: Ibraheem
+"""
+
+#TUTORIAL SET TWO NO 6
+# Program to find the areas of a square.
+# L stands for Length
+
+print("Program to calculate the area of a square.")
+
+L = int(eval(input("Enter length of square: ")))
+
+Area = L**2
+
+print(Area)
+
+
