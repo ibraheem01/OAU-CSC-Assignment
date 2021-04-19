ca78b68bb4fd42d62d1336c7949605af79462ac0
ibraheem01 <66010073+ibraheem01@users.noreply.github.com>
2021-04-19 12:28:51 +0100

    Create Greeting.py

---
+++ Greeting.py
@@ -0,0 +1,15 @@
+#PROGRAM TO GENERATE AN APPROPRIATE GREETING
+
+name = input("Enter your name: ")
+
+#T_O_D stands for time of the day(either morning, afternoon or night)
+
+T_O_D = (input("What time of the day is it: "))
+
+if T_O_D >= 6:00 and T_O_D < 12:00:
+    print("Hi " + name + " and good " + T_O_D)
+
+    elif T_O_D == "afternoon":
+        print("Hi " + name + " and good " + T_O_D)
+
+
