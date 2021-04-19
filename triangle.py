0a8280339e786c0506dc9230f6464fbd58371649
ibraheem01 <66010073+ibraheem01@users.noreply.github.com>
2021-04-19 01:11:39 +0100

    Create triangle.py

---
+++ triangle.py
@@ -0,0 +1,10 @@
+#Program to find the area of a triangle
+# H stands for height of the triangle
+# B stands for the Base of the triangle
+
+print("TO CALCULATE AREA OF A TRIANGLE")
+
+h = eval(input("Enter triangle's height: "))
+b = eval(input("Enter triangle's base: "))
+
+Area = (b*h)/2
