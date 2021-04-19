6003d23a2446c8498497d26ae737431c883da748
ibraheem01 <66010073+ibraheem01@users.noreply.github.com>
2021-04-19 12:28:57 +0100

    Create Temperature Conversion.py

---
+++ Temperature Conversion.py
@@ -0,0 +1,11 @@
+#PROGRAM TO CONVERT TEMPERATURE READING FROM FAHRENHEIT TO CENTIGRADE.
+# C stands for degree centigrade
+# F stands for degree fahrenheit
+
+
+f = eval(input("Enter temperature in Fahrenheit: "))
+
+#formular below
+C=(f-(32))*5/9
+
+print(C)
